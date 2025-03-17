d23c0ade0d6539bd8c72159614f820e7e18d3cb4
Zeybo <sarahvg@hiof.no>
2025-03-17 12:11:06 +0100

    fikset småting

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -1,14 +1,9 @@
 const PageTitle = ({resource}) => {
     return( 
 
-<section id="liste"> 
-<h1>{resource.category}</h1>
 <a href={resource.url}>{resource.title}</a>
-
-</section>
     
-)
-  };
+)};
   
   export default PageTitle;
   
--- src/components/Resources.jsx
+++ src/components/Resources.jsx
@@ -5,15 +5,19 @@ export default function Resources({resources}) {
   const {slug} = useParams()
 
   const categoryProducts = resources.filter((resource) => resource.category === slug)
-    console.log("hjelp", categoryProducts)
+  console.log("hjelp", categoryProducts)
 
   return (
     <>
+    <section id="liste"> 
+    <h1>{categoryProducts[0].category}</h1>
     {categoryProducts.map((resource, index) => (
       <PageTitle
       resource={resource}
       key={`FCT_${index}`}
       />
-  ))}</>
+    ))}
+    </section>
+    </>
   );
 }
